43122d9c74cdfdd11ea77b74d1829b958b787cfc
Davi09-8 <kelompokdua@gmail.com>
2025-04-21 04:05:40 +0700

    Tambah pengumuman

--- app/Http/Controllers/PengumumanController.php
+++ app/Http/Controllers/PengumumanController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pengumuman;
 use Illuminate\Http\Request;
 
 class PengumumanController extends Controller
 {
-    //
+    public function index()
+    {
+        $pengumumans = Pengumuman::orderBy('tanggal', 'desc')->get();
+        return view('pengumuman.index', compact('pengumumans'));
+    }
+
+    public function create()
+    {
+        return view('pengumuman.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'isi' => 'required',
+            'tanggal' => 'required|date',
+        ]);
+
+        Pengumuman::create($request->all());
+        return redirect()->route('pengumuman.index')->with('success', 'Pengumuman berhasil ditambahkan.');
+    }
+
+    public function edit(Pengumuman $pengumuman)
+    {
+        return view('pengumuman.edit', compact('pengumuman'));
+    }
+
+    public function update(Request $request, Pengumuman $pengumuman)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'isi' => 'required',
+            'tanggal' => 'required|date',
+        ]);
+
+        $pengumuman->update($request->all());
+        return redirect()->route('pengumuman.index')->with('success', 'Pengumuman berhasil diperbarui.');
+    }
+
+    public function destroy(Pengumuman $pengumuman)
+    {
+        $pengumuman->delete();
+        return redirect()->route('pengumuman.index')->with('success', 'Pengumuman berhasil dihapus.');
+    }
 }
