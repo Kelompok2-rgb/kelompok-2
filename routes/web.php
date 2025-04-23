b9241ae155bf5cbfc5d59a9d2b1b84093a4daf54
Davi09-8 <kelompokdua@gmail.com>
2025-04-24 01:54:31 +0700

    Menambahkan fitur X atau perbaikan Y

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GaleriController;
+
+Route::resource('galeri', GaleriController::class);
 
 Route::get('/anggota', function () {
     return view('anggota.index');
